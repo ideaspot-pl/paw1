b4a59c5ab001cfb0eca78817f5e7d87a3d96e43a
Artur Karczmarczyk <artur@ideaspot.pl>
2026-09-25 13:22:34 +0200

    PAW1 W01

---
+++ w01-docker-composer/docker/example.js
@@ -0,0 +1,6 @@
+for (i of [5, 10, 15]) {
+    console.log(i);
+}
+
+// cd w01-docker-composer/docker
+// docker run --rm -ti -v "$PWD":/usr/src/app -w /usr/src/app node:20 node example.js
---
+++ w01-docker-composer/docker/index.php
@@ -0,0 +1,5 @@
+<?php phpinfo();
+
+// cd w01-docker-composer/docker
+// docker run --rm -d -p 8123:80 --name=apache-example -v "$PWD":/var/www/html php:8.2-apache
+// docker stop apache-example
